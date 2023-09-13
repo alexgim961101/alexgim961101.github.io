21110d14c1c422be72c252ec1a76a3f3b9ff57cd
alexgim961101 <74600075+alexgim961101@users.noreply.github.com>
2023-09-13 14:29:45 +0900

    Add files via upload

---
+++ js/about.8c507add.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdib"]=self["webpackChunkdib"]||[]).push([[443],{677:function(n,t,u){u.r(t),u.d(t,{default:function(){return r}});var s=function(){var n=this;n._self._c;return n._m(0)},e=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],i=u(1001),a={},l=(0,i.Z)(a,s,e,!1,null,null,null),r=l.exports}}]);
+//# sourceMappingURL=about.8c507add.js.map
